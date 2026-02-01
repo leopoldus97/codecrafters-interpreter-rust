aee28e2f756767be87227754c5ff903c0bdaa7bb
Máté Kiss <34075771+leopoldus97@users.noreply.github.com>
2026-02-01 23:19:07 +0100

    Compiling Expressions (Chapter 17) (#15)
    * Add Pratt's parsing to the compiler
    
    * Fix clippy; fix fmt
    
    * Fix PR comments
    
    * Fix fmt
    
    * Fix PR comments

--- virtual_machine/src/vm.rs
+++ virtual_machine/src/vm.rs
@@ -1,6 +1,10 @@
-use crate::{compiler::Compiler, value::Value};
+use crate::{
+    chunk::{Chunk, OpCode},
+    compiler::Compiler,
+    value::Value,
+};
 
-const _DEBUG_TRACE_EXECUTION: bool = true;
+const DEBUG_TRACE_EXECUTION: bool = true;
 const STACK_MAX: usize = 256;
 
 #[derive(PartialEq)]
@@ -11,6 +15,8 @@ pub enum InterpretResult {
 }
 
 pub struct VM {
+    chunk: Chunk,
+    ip: usize,
     stack: Vec<Value>,
     stack_top: usize,
 }
@@ -18,24 +24,47 @@ pub struct VM {
 impl VM {
     pub fn new() -> Self {
         Self {
+            chunk: Chunk::default(),
+            ip: 0,
             stack: Vec::with_capacity(STACK_MAX),
             stack_top: 0,
         }
     }
 
+    #[inline]
+    fn read_byte(&mut self) -> Option<u8> {
+        let byte = *self.chunk.code().get(self.ip)?;
+        self.ip += 1;
+        Some(byte)
+    }
+
+    #[inline]
+    fn read_constant(&mut self) -> Option<Value> {
+        let index = self.read_byte()?;
+        self.chunk.read_constant(index)
+    }
+
     pub fn interpret(&mut self, source: &str) -> InterpretResult {
+        let mut chunk = Chunk::default();
+
         let mut compiler = Compiler::new(source);
-        compiler.compile()
+
+        if !compiler.compile(&mut chunk) {
+            return InterpretResult::CompileError;
+        }
+
+        self.chunk = chunk;
+        self.ip = 0;
+        self.stack.truncate(0);
+        self.stack_top = 0;
+
+        self.run()
     }
 
     pub fn push(&mut self, value: Value) {
         if self.stack_top >= STACK_MAX {
-            panic!(
-                "Stack overflow: attempted to push beyond STACK_MAX ({})",
-                STACK_MAX
-            );
+            panic!("Stack overflow: attempted to push past STACK_MAX ({STACK_MAX})");
         }
-
         self.stack.push(value);
         self.stack_top += 1;
     }
@@ -46,6 +75,82 @@ impl VM {
         }
         self.stack_top -= 1;
         self.stack.pop()
+    }
+
+    fn run(&mut self) -> InterpretResult {
+        loop {
+            if DEBUG_TRACE_EXECUTION {
+                print!("          ");
+                for slot in &self.stack[..self.stack_top] {
+                    print!("[ {slot} ]");
+                }
+                println!();
+
+                self.chunk.disassemble_instruction(self.ip);
+            }
+
+            let byte = match self.read_byte() {
+                Some(byte) => byte,
+                None => return InterpretResult::RuntimeError,
+            };
+
+            match OpCode::try_from(byte) {
+                Ok(OpCode::OpConstant) => {
+                    let constant = match self.read_constant() {
+                        Some(constant) => constant,
+                        None => return InterpretResult::RuntimeError,
+                    };
+                    self.push(constant);
+                }
+                Ok(OpCode::OpNegate) => {
+                    let value = match self.pop() {
+                        Some(value) => value,
+                        None => return InterpretResult::RuntimeError,
+                    };
+                    self.push(-value);
+                }
+                Ok(OpCode::OpAdd) => {
+                    if self.binary_op(|a, b| a + b).is_none() {
+                        return InterpretResult::RuntimeError;
+                    }
+                }
+                Ok(OpCode::OpSubtract) => {
+                    if self.binary_op(|a, b| a - b).is_none() {
+                        return InterpretResult::RuntimeError;
+                    }
+                }
+                Ok(OpCode::OpMultiply) => {
+                    if self.binary_op(|a, b| a * b).is_none() {
+                        return InterpretResult::RuntimeError;
+                    }
+                }
+                Ok(OpCode::OpDivide) => {
+                    if self.binary_op(|a, b| a / b).is_none() {
+                        return InterpretResult::RuntimeError;
+                    }
+                }
+                Ok(OpCode::OpReturn) => {
+                    let value = match self.pop() {
+                        Some(value) => value,
+                        None => return InterpretResult::RuntimeError,
+                    };
+                    println!("{}", value);
+                    return InterpretResult::Ok;
+                }
+                Err(_) => return InterpretResult::RuntimeError,
+            }
+        }
+    }
+
+    fn binary_op<F>(&mut self, op: F) -> Option<()>
+    where
+        F: Fn(f64, f64) -> f64,
+    {
+        let b = self.pop()?;
+        let a = self.pop()?;
+        self.push(op(a, b));
+
+        Some(())
     }
 }
 
